c1348ed41deaad98d50cd5c33ee33b0e321a9eeb
Milos Zarkovic <mzarkovicm@gmail.com>
2018-04-09 00:22:17 +0200

    initial project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render () {
+    return (
+      <div>This is my App</div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'app/js/main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  mode: 'development'
+}
